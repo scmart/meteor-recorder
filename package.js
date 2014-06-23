3460b9123b99f62414fa21557fce5a6f385f6f03
Sedric <sedric@torsh.co>
2014-06-23 03:57:12 -0700

    explicitly include underscore

--- package.js
+++ package.js
@@ -3,6 +3,7 @@ Package.describe({
 });
 
 Package.on_use(function (api) {
+	api.use(['underscore'], 'client');
 	api.add_files([
 		"recorder/recorderjs/recorder.js",
 		"recorder/wami/swfobject/swfobject.js",
